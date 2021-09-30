cca7d03c2e2b51806bee304d1285e980fd511672
a1216843 <49019204+a1216843@users.noreply.github.com>
2021-10-01 00:30:51 +0900

    2021-10-01

--- app/src/main/java/com/example/clone_insta/LoginActivity.kt
+++ app/src/main/java/com/example/clone_insta/LoginActivity.kt
@@ -3,21 +3,128 @@ package com.example.clone_insta
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    var googleSigninClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 1
+    var callbackManager : CallbackManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
+
+        //이메일, 구글, 페이스북 로그인 버튼 이벤트
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+        google_signin_button.setOnClickListener {
+            googleLogin()
+        }
+        facebook_signin_button.setOnClickListener {
+            facebookLogin()
+        }
+        // 구글 로그인
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        // 구글 로그인 클라이언트 객체
+        googleSigninClient = GoogleSignIn.getClient(this, gso)
+
+        // 콜백 매니저 초기화
+        callbackManager = CallbackManager.Factory.create()
     }
+
+    fun googleLogin(){
+        //signInIntent는 구글 로그인시 출력되는 SubActivity를 말함
+        var signInIntent = googleSigninClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+    fun facebookLogin(){
+        var loginManager = LoginManager.getInstance() //페이스북 로그인 매니저 객체 획득
+        loginManager
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email")) //로그인 매니저를 통해 읽어오기 권한 획득
+
+        // 로그인 매니저에 콜백을 관리하는 callbackManager와 응답 콜백인 FacebookCallback<LoginResult>를 등록
+        loginManager
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+                override fun onSuccess(result: LoginResult?) {
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+            })
+    }
+    fun handleFacebookAccessToken(token : AccessToken?){
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //Creating a user account
+                    moveMainPage(task.result?.user)
+                }else{
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //Creating a user account
+                    moveMainPage(task.result?.user)
+                }else{
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
     fun signinAndSignup(){
         auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
             ?.addOnCompleteListener {
@@ -49,3 +156,6 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 }
+
+// keytool -exportcert -alias AndroidDebugKey -keystore "C:\Users\morta\.android\debug.keystore" | "C:\OpenSSL\bin\openssl" sha1 -binary | "C:\OpenSSL\bin\openssl" base64
+
---
+++ app/src/main/java/com/example/clone_insta/navigation/AlarmFragment.kt
@@ -0,0 +1,17 @@
+package com.example.clone_insta.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.clone_insta.R
+
+class AlarmFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // LayoutInflater는 XML 리소스를 View로 반환하는 역할을 함 주로 사용하는 onCreate의 setContentView도 이러한 Inflater의 역할을 내부적으로 수행한다.
+        // inflate(View 객체로 만들 XML, 객체화 된 View를 담을 레이아웃 or 컨테이너, 바로 인플레이션 할 것인지 여부)
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
+        return view
+    }
+}
